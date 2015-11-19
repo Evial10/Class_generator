a2a8cbd86ecbb4029ec3fd1d0c612d1c3fe54b5c
Dmitry Utkin <evial10@rambler.ru>
2015-11-19 21:05:51 +0300

    Added argument in the method

--- generator.rb
+++ generator.rb
@@ -3,15 +3,15 @@
 class Generator  
   NUM_SYMBOLS = 10 # Symbols number in password
 
-  def self.get_password
+  def self.get_password(num_symbols = NUM_SYMBOLS)    
+    fail ArgumentError, "Expected argument > 0" if num_symbols <= 0    
+    
     vowel = ['a', 'e', 'o', 'u', 'y', 'i']
     consonants = ['a'..'z'].map{ |x| x.to_a }.flatten - vowel
     
-    (0...NUM_SYMBOLS)
+    (0...num_symbols)
     .map { |elem|  elem % 2 == 0 ? consonants[rand(consonants.size)] : vowel[rand(vowel.size)] } 
     .join 
   end
 
 end
-
-
